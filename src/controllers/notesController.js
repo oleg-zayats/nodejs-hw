20231c9f60b1ac08c643bce7541883f2776590f4
Олег <ozaac20@gmail.com>
2026-04-06 22:14:57 +0300

    +

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -1,14 +1,71 @@
 // src/controllers/studentsController.js
+import createHttpError from 'http-errors';
+import { Note } from '../models/note.js';
 
-export const getStudentById = async (req, res) => {
-  const { studentId } = req.params;
-  const student = await student.findById(studentId);
-
-
-  // Додаємо базову обробку помилки замість res.status(404)
-  if (!student) {
-	  throw new Error('Student not found');
+// GET /notes
+export const getAllNotes = async (req, res, next) => {
+  try {
+    const notes = await Note.find();
+    res.status(200).json({
+      status: 200,
+      message: 'Successfully found notes!',
+      data: notes,
+    });
+  } catch (error) {
+    next(error);
   }
+};
 
-  res.status(200).json(student);
+// GET /notes/:noteId
+export const getNoteById = async (req, res, next) => {
+  try {
+    const { noteId } = req.params;
+    const note = await Note.findById(noteId);
+
+    if (!note) {
+      throw createHttpError(404, 'Note not found');
+    }
+
+    res.status(200).json({
+      status: 200,
+      message: `Successfully found note with id ${noteId}!`,
+      data: note,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// POST /notes
+export const createNote = async (req, res, next) => {
+  try {
+    const note = await Note.create(req.body);
+    res.status(201).json({
+      status: 201,
+      message: 'Successfully created a note!',
+      data: note,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// DELETE /notes/:noteId
+export const deleteNote = async (req, res, next) => {
+  try {
+    const { noteId } = req.params;
+    const note = await Note.findByIdAndDelete(noteId);
+
+    if (!note) {
+      throw createHttpError(404, 'Note not found');
+    }
+
+    res.status(200).json({
+      status: 200,
+      message: 'Successfully deleted the note!',
+      data: note,
+    });
+  } catch (error) {
+    next(error);
+  }
 };
--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -2,7 +2,7 @@
 
 import { Router } from 'express';
 import { student } from '../models/note.js';
-
+import * as notesController from '../controllers/notesController.js';
 const router = Router();
 
 router.get('/students', async (req, res) => {
@@ -10,6 +10,12 @@ router.get('/students', async (req, res) => {
   res.status(200).json(students);
 });
 
+
+
+router.get('/notes', notesController.getAllNotes);
+router.get('/notes/:noteId', notesController.getNoteById);
+router.post('/notes', notesController.createNote);
+router.delete('/notes/:noteId', notesController.deleteNote);
 router.get('/students/:studentId', async (req, res) => {
   const { studentId } = req.params;
   const student = await student.findById(studentId);
