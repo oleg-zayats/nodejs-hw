ee4a1e0b5668ba76d3ee05cfe6f224e674a3970e
Олег <ozaac20@gmail.com>
2026-04-07 15:29:55 +0300

    fix11

--- src/server.js
+++ src/server.js
@@ -4,10 +4,10 @@ import 'dotenv/config';
 
 
 import { connectMongoDB } from './db/connectMongoDB.js';
-import { logger } from '../middleware/logger.js';
-import { notFoundHandler } from '../middleware/notFoundHandler.js';
-import { errorHandler } from '../middleware/errorHandler.js';
-import notesRouter from '../routes/notesRoutes.js';
+import { logger } from './middleware/logger.js';
+import { notFoundHandler } from './middleware/notFoundHandler.js';
+import { errorHandler } from './middleware/errorHandler.js';
+import notesRouter from './routes/notesRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -18,10 +18,12 @@ app.use(express.json()); // 2. Парсинг JSON-тіла
 app.use(cors());         // 3. Дозвіл для запитів з інших доменів
 // ...тут ваші маршрути
 app.use(notesRouter);
-app.use(notFoundHandler);
-app.use(errorHandler);
 // 404 — якщо маршрут не знайдено
+
+
 app.use(notFoundHandler);
+
+
 
 // Error — якщо під час запиту виникла помилка
 app.use(errorHandler);
