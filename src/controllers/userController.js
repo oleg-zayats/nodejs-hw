9b6843b2bd728b6f62bc4abedfb4f7f25e6bab30
Олег <ozaac20@gmail.com>
2026-04-21 19:13:54 +0300

    f

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -12,7 +12,7 @@ export const updateUserAvatar = async (req, res) => {
   const updatedUser = await User.findOneAndUpdate(
     { _id: req.user._id },
     { avatar: result.secure_url },
-    { new: true },
+    { returnDocument: 'after' },
   );
 
   res.status(200).json({ url: updatedUser.avatar });
