99faad9f7bfeb407e2e1a593b3f73fa8be92b124
Олег <ozaac20@gmail.com>
2026-04-21 17:50:42 +0300

    +

--- src/server.js
+++ src/server.js
@@ -1,12 +1,16 @@
-import "dotenv/config";
-import express from "express";
-import cors from "cors";
-import { errors } from "celebrate";
-import { connectMongoDB } from "./db/connectMongoDB.js";
-import { logger } from "./middleware/logger.js";
-import { notFoundHandler } from "./middleware/notFoundHandler.js";
-import { errorHandler } from "./middleware/errorHandler.js";
-import notesRoutes from "./routes/notesRoutes.js";
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import { errors } from 'celebrate';
+
+import { connectMongoDB } from './db/connectMongoDB.js';
+import { logger } from './middleware/logger.js';
+import { notFoundHandler } from './middleware/notFoundHandler.js';
+import { errorHandler } from './middleware/errorHandler.js';
+
+import notesRoutes from './routes/notesRoutes.js';
+import authRoutes from './routes/authRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
@@ -14,11 +18,12 @@ const PORT = process.env.PORT ?? 3000;
 app.use(logger);
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
 
+app.use(authRoutes);
 app.use(notesRoutes);
 
 app.use(notFoundHandler);
-
 app.use(errors());
 
 app.use(errorHandler);
